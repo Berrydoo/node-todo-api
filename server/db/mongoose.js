0538935ff68c16f9711f8bd180ce39dd87347fd3
Berrydoo <Jim.dewberry@Gmail.com>
2016-11-05 17:34:55 -0400

    set up separate dev and test environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 // this is the URI for the database connections
 //mongodb://heroku_qz32lbjw:4g8ion60ai0ieq8arfr3akgl8t@ds019856.mlab.com:19856/heroku_qz32lbjw
